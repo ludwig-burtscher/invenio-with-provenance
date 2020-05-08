579716394493c0259207260caac835bb7c8ed740
Ludwig Burtscher <ludwig.burtscher@outlook.com>
2020-05-08 13:06:28 +0200

    fix invenio setup

--- invenio-rdm/setup-invenio.sh
+++ invenio-rdm/setup-invenio.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
+function fail {
+    printf '%s\n' "$1" >&2
+    exit 1
+}
 
-# install invenio-cli and setup dependencies
-pip install pyyaml
-pipenv install invenio-cli
 
-# init invenio
-invenio-cli init --flavour=RDM
+echo "Install invenio-cli"
+pip3 install invenio-cli || fail "Could not install invenio-cli"
+export PATH=$PATH:~/.local/bin
+
+echo "Init invenio"
+invenio-cli init --flavour=RDM || fail "Could not init invenio"
 
 # get invenio base directory in $directory
 echo "Please select invenio directory:"
 select directory in */; do break; done
 
-# copy certificate
+echo "Copy certificate"
 cp cert/* $directory/docker/nginx/
 
-# modify docker setup files
+echo "Modify docker setup files"
 chmod +x ./setup/docker/modify-docker-compose.py
-./setup/docker/modify-docker-compose.py $directory
+python3 setup/docker/modify-docker-compose.py $directory || fail "Could not modify docker-compose"
 
 cd $directory
 
-# invenio start
-invenio-cli containerize
-invenio-cli demo --containers
+echo "Fix dependency versions"
+pip3 uninstall -y Werkzeug
+pip3 install Werkzeug==0.16.1 jsonresolver==0.2.1
+
+echo "Invenio start"
+invenio-cli containerize || fail "Could not generate containers"
+echo "Adding demo data"
+invenio-cli demo --containers || fail "Could not add demo data"
+
+echo "Setup finished"
