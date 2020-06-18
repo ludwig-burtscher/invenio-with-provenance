6e6e5edf3b56f5bc38d7df5a5d133575cf3f0a29
Coil <coilspan@gmail.com>
2020-06-18 18:26:06 +0200

    Add testdata generator

---
+++ tests/invenio-api.sh
@@ -0,0 +1,139 @@
+#!/usr/bin/env bash
+
+# This file contains function to interact with an invenio installation
+# Dependencies curl
+
+HOST="http://localhost"
+CURL_OPTS=""
+SESSION=""
+
+fail () {
+  printf '%s\n' "$1" >&2
+  exit 1
+}
+
+check_deps () {
+  [ -x "$(command -v curl)" ] || fail "curl is not installed; try: apt install curl"
+  [ -x "$(command -v jq)" ] || fail "jq is not installed; try: apt install jq"
+  [ -x "$(command -v grep)" ] || fail "grep is not installed"
+  [ -x "$(command -v cut)" ] || fail "cut is not installed"
+}
+
+set_invenio_host () {
+  HOST="$1"
+}
+
+set_curl_opts () {
+  CURL_OPTS="$1"
+}
+
+set_session () {
+  SESSION="$1"
+}
+
+create_record () {
+  record_model="$1"
+  curl ${CURL_OPTS} -s -b session=${SESSION} --header "Content-Type: application/json" \
+    --request POST \
+    --data "$record_model" \
+    "$HOST/api/records/" | jq -r ".id"
+}
+
+create_record_simple () {
+   title="$1"
+   read -d '' txt << EOF
+{
+  "_access": {
+        "metadata_restricted": false,
+        "files_restricted": false
+    },
+    "_owners": [1],
+    "_created_by": 1,
+    "access_right": "open",
+    "resource_type": {
+        "type": "publication",
+        "subtype": "publication-article"
+    },
+    "identifiers": {
+        "DOI": "10.9999/rdm.9999999",
+        "arXiv": "9999.99999"
+    },
+    "creators": [
+        {
+            "name": "Julio Cesar",
+            "type": "Personal",
+            "given_name": "Julio",
+            "family_name": "Cesar",
+            "identifiers": {
+                "Orcid": "9999-9999-9999-9999"
+            },
+            "affiliations": [
+                {
+                    "name": "Entity One",
+                    "identifier": "entity-one",
+                    "scheme": "entity-id-scheme"
+                }
+            ]
+        }
+    ],
+    "titles": [
+        {
+            "title": "$title",
+            "type": "Other",
+            "lang": "eng"
+        }
+    ],
+    "descriptions": [
+        {
+            "description": "A story on how Julio Cesar relates to Gladiator.",
+            "type": "Abstract",
+            "lang": "eng"
+        }
+    ],
+    "licenses": [
+        {
+            "license": "Berkeley Software Distribution 3",
+            "uri": "https://opensource.org/licenses/BSD-3-Clause",
+            "identifier": "BSD-3",
+            "scheme": "BSD-3"
+        }
+    ]
+}
+EOF
+create_record "$txt"
+}
+
+search_records () {
+  querystr="$1"
+
+  curl ${CURL_OPTS} -s -b session=${SESSION} "$HOST/api/records/?q=$querystr" | jq -r ".hits.hits | map(.id) | .[]"
+}
+
+upload_file_to_record () {
+  record_id="$1"
+  filename="$2"
+
+  curl ${CURL_OPTS} -s -b session=${SESSION} -H "Content-Type: application/octet-stream" \
+    --request PUT \
+    --data-binary @${filename} \
+    "$HOST/api/records/$record_id/files/$filename" > /dev/null
+}
+
+read_file_for_record () {
+  record_id="$1"
+  filename="$2"
+  curl ${CURL_OPTS} -b session=${SESSION} "$HOST/api/records/$record_id/files/$filename"
+}
+
+signup () {
+  email="$1"
+  password="$2"
+
+  cookiefile=$(mktemp)
+
+  csrf_token=$(curl -c ${cookiefile} -s ${CURL_OPTS} "$HOST/signup/" | grep -e "csrf_token" | grep -oE "[A-Za-z0-9._/-]{30,}")
+  curl -b ${cookiefile} -c ${cookiefile} -s ${CURL_OPTS} -X POST --data "csrf_token=${csrf_token}&email=${email}&password=${password}" "$HOST/signup/" > /dev/null
+
+  cat $cookiefile | grep "HttpOnly" | grep "session" | cut -f7 # Return session cookie
+  rm ${cookiefile}
+}
---
+++ tests/test-szenario.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+source ./invenio-api.sh
+check_deps
+
+set_invenio_host "https://10.0.0.160"
+set_curl_opts "--insecure"
+
+# use a user
+set_session $(signup "test${RANDOM}@test.it" "passdfsdfgsdfg")
+
+# to logout use:
+# set_session ""
+
+id=$(create_record_simple "Testdata")
+echo "Created test record with new key: $id"
+
+querystr="Testdata"
+echo "Searching for '$querystr'"
+# search_records "$querystr"
+
+testfilename="test_upload_file.txt"
+touch ${testfilename}
+echo -e "# This is Test content\n[Maybe]\nItsA = ini" > ${testfilename}
+upload_file_to_record ${id} ${testfilename}
+
+echo "Reading file"
+read_file_for_record ${id} ${testfilename}
+echo
